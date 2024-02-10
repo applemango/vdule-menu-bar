97e5b551250388b862e6386c453e15e979bc4a3f
apple <apple.mangoooo97@gmail.com>
2024-02-10 14:54:27 +0900

    add type

---
+++ vdule/struct.swift
@@ -0,0 +1,43 @@
+//
+//  struct.swift
+//  vdule
+//
+//  Created by apple on 2024/02/10.
+//
+
+import Foundation
+
+struct Channel: Codable {
+    var id: String
+    var name: String
+    var description: String
+    var handle: String
+    var publish_at: String
+    var icon_image: String
+    var uploads_playlist: String
+    var view_count: Int
+    var subscriber_count: Int
+    var video_count: Int
+    var trailer: String
+    var banner_image: String
+}
+
+struct ScheduleDate: Codable {
+    var year: Int
+    var month: Int
+    var day: Int
+    var hour: Int? = 0
+    var minute: Int? = 0
+}
+
+struct Schedule: Codable {
+    var id: String
+    var title: String
+    var thumbnail: String
+    var date: ScheduleDate
+    var channel: Channel
+}
+
+struct RawResponseSchedule: Codable {
+    var videos: [Schedule]
+}
