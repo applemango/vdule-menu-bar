93f3c189098a6ef9c3086f2add9b9d03f18b218a
apple <apple.mangoooo97@gmail.com>
2024-02-10 10:23:52 +0900

    Initial Commit

---
+++ vdule/vduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  vduleApp.swift
+//  vdule
+//
+//  Created by apple on 2024/02/10.
+//
+
+import SwiftUI
+
+@main
+struct vduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
