56c5450b8be0a266f0f72679c849d31c61813e3a
apple <apple.mangoooo97@gmail.com>
2024-02-10 18:39:45 +0900

    add

--- vdule/MenuView.swift
+++ vdule/MenuView.swift
@@ -11,8 +11,46 @@ import SwiftUI
 
 struct MenuView: View {
     @ObservedObject var loader: LoadSchedule
+    @Environment(\.openURL) private var openURL
     var body: some View {
-        Text("Hello, Swift!")
+        let util = ScheduleUtils(schedules: loader.schedules)
+        Label("Live Schedule", systemImage: "")
+        ForEach(util.getEventAfter(), id: \.id) { s in
+            let title = getLiveTitle(schedule: s)
+            let imageUrl = URL(string: "https://s2.i32.jp/?url=\(s.channel.icon_image)")
+            Button(action: {
+                if let url = URL(string: "https://www.youtube.com/watch?v=\(s.id)") {
+                    openURL(url)
+                }
+            }) {
+                HStack {
+                    AsyncImage(url: imageUrl) { image in
+                        image.resizable()
+                    } placeholder: {
+                        ProgressView()
+                    }.frame(width: 30, height: 30).id(imageUrl)
+                    Text(title)
+                }
+            }.keyboardShortcut("o")
+        }
+        Divider()
+        Label("Website and settings", systemImage: "")
+        Button(action: {}) {
+            HStack {
+                Image(systemName: "gearshape")
+                Text("Settings")
+            }
+        }.keyboardShortcut("s")
+        Button(action: {
+            if let url = URL(string: "http://127.0.0.1:3001") {
+                openURL(url)
+            }
+        }) {
+            HStack {
+                Image(systemName: "network")
+                Text("Website")
+            }
+        }.keyboardShortcut("o")
     }
 }
 
